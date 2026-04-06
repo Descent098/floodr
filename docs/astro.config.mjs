f370e864fe3ae67f45b60ba7f9afd66c3cba90da
Kieran Wood <kieran@canadiancoding.ca>
2026-04-05 20:38:01 -0600

    Docs: Started user docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,27 +1,91 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import mermaid from 'astro-mermaid';
+
+// Starlight plugins
+import starlightUiTweaks from 'starlight-ui-tweaks'
+import starlightSidebarSwipe from 'starlight-sidebar-swipe'
+import starlightSidebarTopics from 'starlight-sidebar-topics'
+import starlightContextualMenu from "starlight-contextual-menu";
 
 // https://astro.build/config
 export default defineConfig({
+	base: "/floodr",
+	site: 'https://kieranwood.ca',
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
+			title: 'Floodr',
+			customCss: [
+				'./src/theme.css',
+			],
+			social: [
 				{
-					label: 'Getting Started',
-					autogenerate: { directory: 'getting-started' },
-					// items: [
-					// 	// Each item here is one entry in the navigation menu.
-					// 	// { label: 'Example Guide', slug: 'guides/example' },
-					// ],
-				},
-				{
-					label: 'Benchmark Reference',
-					autogenerate: { directory: 'benchmark-reference' },
-				},
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/descent098/floodr'
+				}
+			],
+			plugins: [
+				starlightUiTweaks({
+					navbarLinks: [
+						{ 
+							label: "Getting Started", 
+							href: "/floodr/getting-started" 
+						},
+						{ 
+							label: "CLI", 
+							href: "/floodr/cli" 
+						},
+						// { label: "Library", href: "/floodr/utilities" }, // TODO: Add crate URL when it's available
+					],
+				}),
+				// Enables swiping in the menu from the side on moble
+				starlightSidebarSwipe(),
+
+				// Enables view-as and copy-page options
+				starlightContextualMenu({
+					actions: ["copy", "view", "chatgpt", "claude"]
+				}),
+
+
+				starlightSidebarTopics([
+					{
+						label: "Basics",
+						link: "/floodr/getting-started",
+						icon: "seti:html",
+						items: [
+							{
+								label: 'Gettting started',
+								autogenerate: { directory: "getting-starte" },
+							},
+							{
+								label: 'Command Line Interface',
+								autogenerate: { directory: "cli" },
+							},
+						],
+
+					},
+
+					{
+						label: "Reference",
+						link: "/floodr/benchmark-reference",
+						icon: "seti:html",
+						items: [
+							{
+								label: 'Benchmark File Reference',
+								autogenerate: { directory: "benchmark-reference" },
+							},
+						],
+
+					},
+				],
+				), // end of sidebar config
 			],
 		}),
+		mermaid({
+			theme: "default",
+			autoTheme: true
+		})
 	],
 });
